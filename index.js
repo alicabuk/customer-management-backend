1679f131d710c2c964ce596f1e92fcbdb607951e
Ali <muhalicabuk@gmail.com>
2025-05-10 13:24:17 +0300

    Started server and tested login endpoint

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const routes = require('./routes');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+
+app.use('/api', routes);
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
